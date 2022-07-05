9b07c851ba18e87d35564bf4bba5391a7af508c7
George Austin <george@georges-mbp.myfiosgateway.com>
2022-07-05 01:07:22 -0400

    adressing memory error on GHA windows

--- tests/testthat/test-plasma.R
+++ tests/testthat/test-plasma.R
@@ -6,17 +6,18 @@ context('test-plasma.R')
 
 test_that(desc = 'testing SCRuB variations on plasma dataset',
           {
-            
-            data <- (read.csv( paste0( test_path(), '/plasma_data.csv'), row.names=1) %>% as.matrix() )[,1:500]
+            n_feats_considered=100 # to avoid memory errors on GHA
+            data <- (read.csv( paste0( test_path(), '/plasma_data.csv'), row.names=1) %>% as.matrix() )[,1:n_feats_considered]
             metadata <- read.csv( paste0( test_path(), '/plasma_metadata.csv'), row.names=1)
             
             set.seed(1)
+            
             print('Testing full samples, metadata....')
-            scr_out_1 <- SCRuB( data[, 1:500], metadata, c("control blank DNA extraction", "control blank library prep") )
+            scr_out_1 <- SCRuB( data[, 1:n_feats_considered], metadata, c("control blank DNA extraction", "control blank library prep") )
             print('Testing metadata without spatial information, only control blank library prep')
-            scr_out_2 <- SCRuB( data[, 1:500], metadata[,1:2], "control blank library prep") 
+            scr_out_2 <- SCRuB( data[, 1:n_feats_considered], metadata[,1:2], "control blank library prep") 
             print('Testing shortened data')
-            scr_out_3 <- SCRuB( data[1:50, 1:500], metadata[1:50,], c("control blank DNA extraction") )
+            scr_out_3 <- SCRuB( data[1:50, 1:n_feats_considered], metadata[1:50,], c("control blank DNA extraction") )
             
             
             expect_type(scr_out_1, 'list')
@@ -24,11 +25,11 @@ test_that(desc = 'testing SCRuB variations on plasma dataset',
             
             expect_true( nrow(scr_out_1$decontaminated_samples) == sum( F == metadata$is_control ) )
             expect_true( nrow(scr_out_1$decontaminated_samples) == length(scr_out_1$p))
-            expect_true( length(scr_out_1$inner_iterations$`control blank DNA extraction`$gamma) == 500 )
+            expect_true( length(scr_out_1$inner_iterations$`control blank DNA extraction`$gamma) == n_feats_considered )
             expect_true( sum(scr_out_1$inner_iterations$`control blank DNA extraction`$gamma) %>% round(5) == 1 )
             expect_true( scr_out_1$inner_iterations$`control blank DNA extraction`$alpha %>% rowSums() %>% mean() %>% round(5) == 1 )
-            expect_true( min( colnames(data)[1:500] == colnames(scr_out_1$decontaminated_samples)) == T )
-            expect_true( min( colnames(data)[1:500] == colnames(scr_out_2$decontaminated_samples)) == T )
+            expect_true( min( colnames(data)[1:n_feats_considered] == colnames(scr_out_1$decontaminated_samples)) == T )
+            expect_true( min( colnames(data)[1:n_feats_considered] == colnames(scr_out_2$decontaminated_samples)) == T )
           })
 
 
